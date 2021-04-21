e1fe82071a05298554594014324b9a48a0427645
DESKTOP-9MFU7NQ\yl <1269123897@qq.com>
2021-04-21 17:53:07 +0800

    2021/04/21 17：52 buildPattern commit

---
+++ designPatterns/src/main/java/cn/seeyourface/designpatterns/buildPattern/Packing.java
@@ -0,0 +1,12 @@
+package cn.seeyourface.designpatterns.buildPattern;
+
+/**
+ * @ClassName: Packing
+ * @Description: TODO
+ * @Author: Yanglei
+ * @Date: 2021/4/21 17:01
+ * @Version: V1.0
+ */
+public interface Packing {
+    public String pack();
+}
